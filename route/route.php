3d8d9fc91f32459d3c23cb07f5bb44341b5371df
Víctor Cerrato <10145832+viclucese@users.noreply.github.com>
2024-10-09 14:24:45 +0200

    actualizacion
    añadidos más mensajes y selector por tipo de ruta

--- route/route.php
+++ route/route.php
@@ -11,9 +11,22 @@
     //yo lo tengo en 1 porque tengo un nivel menos de directorio
 
     if(count(array_filter($arrayRutas)) == 1) { 
-        $json = array("detalle"=>"no encontrado");
+        $json = array("Mensaje"=>"No hay API");
         echo json_encode($json,true);
         return;
+    } else {
+        if(count(array_filter($arrayRutas)) == 2) { 
+            if(array_filter($arrayRutas)[2] == "cursos") {
+                $json = array("detalle"=>"estas en la vista cursos");
+                echo json_encode($json,true);
+                return;
+            }
+            if(array_filter($arrayRutas)[2] == "registro") {
+                $json = array("detalle"=>"estas en la vista registro");
+                echo json_encode($json,true);
+                return;
+            }            
+        }
     }
     
 
