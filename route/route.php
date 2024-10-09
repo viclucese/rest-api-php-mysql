6fcdbee1a93edf0dcfbaaeb5096b9397b1f97e43
Víctor Cerrato <10145832+viclucese@users.noreply.github.com>
2024-10-09 09:47:03 +0200

    update

--- route/route.php
+++ route/route.php
@@ -1,5 +1,20 @@
 <?php
 
-echo "Soy la ruta";
+    $arrayRutas = explode("/",$_SERVER['REQUEST_URI']);
+    echo $_SERVER['REQUEST_URI'];
+
+    echo "<pre>";
+    print_r($arrayRutas);
+    echo "</pre>";
+
+    //en el ejemplo se compara con 2
+    //yo lo tengo en 1 porque tengo un nivel menos de directorio
+
+    if(count(array_filter($arrayRutas)) == 1) { 
+        $json = array("detalle"=>"no encontrado");
+        echo json_encode($json,true);
+        return;
+    }
+    
 
 ?>
